daa3a5e8e0e1616be083690dbefa2e660c890862
Maximilian Scheler <scheler.max@googlemail.com>
2022-06-23 14:32:05 +0200

    :arrow_up: Upgrade dependencies and adjust imports

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
-const glob = require('globby');
-const { simpleParser } = require('mailparser');
-const jsdom = require('jsdom');
+import { globby } from "globby";
+import { simpleParser } from "mailparser";
+import jsdom from "jsdom";
 const { JSDOM } = jsdom;
 
-const fs = require('fs');
-const path = require('path');
+import fs from "fs";
+import path from "path";
 
 // Save our links in a `links.txt` file
-const stream = fs.createWriteStream('links.txt', { flags: 'a' });
+const stream = fs.createWriteStream("links.txt", { flags: "a" });
 
-glob('./emails/*.eml').then(files => {
-    console.log(`Found ${files.length} EML files. Enumerating them now...`);
-    let index = 0;
+const __dirname = path.resolve(path.dirname(""));
 
-    files.forEach(file => {
-        let full_path = path.join(__dirname, file);
-        let eml_buffer = fs.readFileSync(full_path);
+globby("./emails/*.eml").then((files) => {
+  console.log(`Found ${files.length} EML files. Enumerating them now...`);
+  let index = 0;
 
-        simpleParser(eml_buffer, (err, mail) => {
-            if (err) {
-                console.log('ERRRROR');
-                console.log(err);
-                process.exit(1);
-            }
+  files.forEach((file) => {
+    let full_path = path.join(__dirname, file);
+    let eml_buffer = fs.readFileSync(full_path);
 
-            // Use an index counter since this callback doesn't run synchronously
-            console.log(`${++index} / ${files.length}: ${file}`);
+    simpleParser(eml_buffer, (err, mail) => {
+      if (err) {
+        console.log("ERRRROR");
+        console.log(err);
+        process.exit(1);
+      }
 
-            // Build a DOM from our email's HTML so we can query for `<a>` tags
-            let dom = new JSDOM(mail.html);
-            let links = dom.window.document.querySelectorAll('a');
+      // Use an index counter since this callback doesn't run synchronously
+      console.log(`${++index} / ${files.length}: ${file}`);
 
-            stream.write('\n' + '###### ' + file + ' ######' + '\n');
-            for (let i = 0; i < links.length; i++) {
-                // Write our links out to our file, 'links.txt'
-                stream.write(links[i].href.toString() + '\n');
-            }
-        });
+      // Build a DOM from our email's HTML so we can query for `<a>` tags
+      let dom = new JSDOM(mail.html);
+      let links = dom.window.document.querySelectorAll("a");
+
+      stream.write("\n" + "###### " + file + " ######" + "\n");
+      for (let i = 0; i < links.length; i++) {
+        // Write our links out to our file, 'links.txt'
+        stream.write(links[i].href.toString() + "\n");
+      }
     });
+  });
 });
